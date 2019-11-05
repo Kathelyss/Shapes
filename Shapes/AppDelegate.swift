178e4d4d2172145cad6c3ed6867e2df65d2ae08b
Ekaterina Ryzhova <rubicante@icloud.com>
2019-11-05 22:40:07 +0300

    Initial Commit

---
+++ Shapes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Shapes
+//
+//  Created by Ekaterina Ryzhova on 05.11.2019.
+//  Copyright © 2019 Ekaterina Ryzhova. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
